569afba09dc97e91efa2911e57777273525d6520
Siddheshwar1895 <152240372+Siddheshwar1895@users.noreply.github.com>
2025-06-19 13:34:49 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = 5000;
-
-app.get('/', (req, res) => {
-  res.send('Ludo Game Backend - Coming Soon...');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+console.log('Ludo backend running');
